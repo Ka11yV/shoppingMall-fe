d6c6d01f035344f534e41442865f0f473df02b5b
Kalimous <elsword485667@gmail.com>
2024-06-14 01:54:45 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import AppLayout from "./Layout/AppLayout";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
-  return (
-    <div>
-      <AppLayout>
-        <AppRouter />
-      </AppLayout>
-    </div>
-  );
+    const good = "good";
+    return (
+        <div>
+            <AppLayout>
+                <AppRouter />
+            </AppLayout>
+        </div>
+    );
 }
 
 export default App;
